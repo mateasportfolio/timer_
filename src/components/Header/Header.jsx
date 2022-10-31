0f1fb00f446bf11d4d3a469058433b511a7ba641
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-10-31 12:18:27 +0100

    deleted style.css from header.jsx and used chakra ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ import "./style.css";
 export const Header = () => {
   return (
     <div className="header">
-      <Text fontSize="3xl" textAlign="center">
+      <Text fontSize="3xl" textAlign="center" p="2">
         Meditation-Timer
       </Text>
       <div className="copy_text">
-        <Text colorScheme="purple">
+        <Text p="10" colorScheme="purple">
           Create your own meditation timers Meditate following our simple guided
           meditations and breathing exercises
         </Text>

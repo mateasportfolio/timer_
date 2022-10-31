6cd9a61128ca3d4e649bcc8fc43b81159f2a486e
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-10-31 12:44:09 +0100

    add chakra ui style props in ImageGrid.jsx

--- src/components/ImageGrid/ImageGrid.jsx
+++ src/components/ImageGrid/ImageGrid.jsx
@@ -1,30 +1,27 @@
 import { Grid, GridItem, Image } from "@chakra-ui/react";
 import React from "react";
 import { auroraboralis, pink, sterne, tree } from "../../assets";
-import "./style.css";
 
 export const ImageGrid = () => {
   return (
-    <div className="image__container">
-      <Grid
-        h="200px"
-        templateRows="repeat(2, 1fr)"
-        templateColumns="repeat(5, 1fr)"
-        gap={4}
-      >
-        <GridItem rowSpan={2} colSpan={1} className="grid-item">
-          <Image src={tree} objectFit="cover" className="grid-img" />
-        </GridItem>
-        <GridItem rowSpan={1} colSpan={2} className="grid-item">
-          <Image src={sterne} objectFit="cover" className="grid-img" />
-        </GridItem>
-        <GridItem rowSpan={1} colSpan={2} className="grid-item">
-          <Image src={auroraboralis} objectFit="cover" className="grid-img" />
-        </GridItem>
-        <GridItem rowSpan={1} colSpan={4} className="grid-item">
-          <Image src={pink} objectFit="cover" className="grid-img" />
-        </GridItem>
-      </Grid>
-    </div>
+    <Grid
+      h="200px"
+      templateRows="repeat(2, 1fr)"
+      templateColumns="repeat(5, 1fr)"
+      gap={4}
+    >
+      <GridItem rowSpan={2} colSpan={1} h="100%" w="100%" overflow="hidden">
+        <Image src={tree} objectFit="cover" h="100%" w="100%" />
+      </GridItem>
+      <GridItem rowSpan={1} colSpan={2} h="100%" w="100%" overflow="hidden">
+        <Image src={sterne} objectFit="cover" h="100%" w="100%" />
+      </GridItem>
+      <GridItem rowSpan={1} colSpan={2} h="100%" w="100%" overflow="hidden">
+        <Image src={auroraboralis} objectFit="cover" h="100%" w="100%" />
+      </GridItem>
+      <GridItem rowSpan={1} colSpan={4} h="100%" w="100%" overflow="hidden">
+        <Image src={pink} objectFit="cover" h="100%" w="100%" />
+      </GridItem>
+    </Grid>
   );
 };

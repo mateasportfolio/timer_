be4c6ebcd334a742a78922e7eab1d882bc631a69
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-11-04 09:52:10 +0100

    add padding in Description component

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 export default function Footer() {
   return (
     <div className="footer">
-      <Text mt={20} fontSize="l" color="whiteAlpha.900">
+      <Text mt={20} fontSize="l" color="whiteAlpha.900" p="30px">
         “Enlightenment is not an experience. It is the state where you are left
         absolutely alone, nothing to know. No object, however beautiful, is
         present. Only in that moment does your consciousness, unobstructed by
